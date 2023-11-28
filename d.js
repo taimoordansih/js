b251864a6b5c40bb8f5cd21d1ea279c89d82575d
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2023-11-28 13:04:33 +0000

    day2

--- d.js
+++ d.js
@@ -1,3 +1,19 @@
 let account
-let city = "lahore"
+let name = "danish";
+let clas = "1st year";
+let city = "lahore";
 console.table([account, city])
+console.log(`may name is ${name} i am in class ${clas}`)
+const gameName = new String("assassin creed")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf("c"))
+const newString = gameName.substring(0, 4)
+console.log(newString)
+const score = 400
+const balance = new Number(100)
+console.log(score)
+console.log(balance)
