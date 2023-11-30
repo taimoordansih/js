f702851a2df81e383f68a4bbd0ed187c77cc94b0
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2023-11-30 11:21:03 +0000

    dan

---
+++ t.js
@@ -0,0 +1,8 @@
+const balance = new Number(100)
+console.log(balance.toFixed(2))
+console.log(Math)
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1) + min))
+
