00929bfce8653d43b0c5c8e9c21089d6557fae4e
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2023-12-01 08:10:06 +0000

    arr

---
+++ arr.js
@@ -0,0 +1,28 @@
+// // console.log[`ther are many many super heros movies like ${dchero} and ${marvel}`]
+// // const myarr = [1, 2, 3, 4, 5,]
+// // myarr.push(6)
+// // myarr.push(7)
+// // myarr.unshift(1)
+// // // myarr.shift() unshift for ad something in start and shift is remove first thing
+// // console.log(myarr)
+
+// const dchero = ["superman", "batman" , "flash"];
+// const marvel = ["ironman", "captain america", "spider man"];
+// // dchero.push(marvel)
+// // const all_heros = dchero.concat(marvel); 
+// const all_new_heros = [...dchero,...marvel];
+// console.log(all_new_heros);
+// const another_arr = [1, 2, 3, [4, [2]], 5, [5,[6,[8,6]]]]
+// const real_another_arr = another_arr.flat(Infinity)
+// console.log(another_arr)
+// console.log(real_another_arr)
+/* agar hamein pochna ho ke koi eliment array h k nai tu*/
+console.log(Array.isArray("dansih"))
+console.log(Array.from("dansih"))
+// console.log(Array.from({name : "danish"})) yehan per ye khali [] deta ha kyun ke name is ki key h or danish is ki value to hamne ye difine nahi kiya ke kis ko arr me convert karna h
+// const score = 100
+// const score2 = 200
+// const score3 = 300
+// console.log(Array.of(score, score2, score3))
+
+
