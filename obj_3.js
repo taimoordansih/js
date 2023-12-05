b920fb4c3dc41d83fc70d2b1b2751622e4014ea1
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2023-12-05 12:18:00 +0000

    object_3_destructure_and_A.P.I

---
+++ obj_3.js
@@ -0,0 +1,32 @@
+                    /*---------------------object destructure--------------------------*/
+
+const course = {
+    course : "js in hindi",
+    price : "999",
+    courseinstructor : "taimoor-danish",
+    // course.courseinstructor
+}                    
+    
+// const {courseinstructor} = course
+/*-------------hum isko special nam bi de sakte hein----------*/
+/*-------------{} before = destruction------------------------*/
+const {courseinstructor : instructor} = course
+
+console.log(instructor)
+
+
+
+
+
+
+
+
+
+
+/*--------------------------------------------------------------------------------------------------*/
+*/--------------------------------for sample A.P.I response------------------------*/ 
+/*-------------------------------randomyserme A.P.I---------------------------------*/
+/*-------------------------------jason formator tool for A.P.I------------------------*/
+/*------------------------------/ A.P.I writes in json format {},[]----------------------*/
+{}
+[]
