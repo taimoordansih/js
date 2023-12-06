9ff7a491856e90b1ecb4119ca7d0818df9b37576
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2023-12-06 07:29:24 +0000

    function_1

---
+++ func_1.js
@@ -0,0 +1,24 @@
+function SayMyName () {
+
+    console.log("d");
+    console.log("a");
+    console.log("n");
+    console.log("i");
+    console.log("s");
+    console.log("h");
+}
+SayMyName()
+/* Function"name"(){} mein () ke data ko perameters boltien h
+function ko exicute karne ke liye "name"() likhtein h is mein () ke ander data ko perameters boltein h
+function key world h
+name 
+() function ka syntax h
+or complete function ka defination h*/
+
+/*--------------------------------------------------------------------------------------------------- */
+
+function AddTwoNumbers (number1, number2){
+    console.log( number1+number2 )
+
+}
+AddTwoNumbers(4,5)
