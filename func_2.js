1b29746985df74172cec207391bafad2479cabea
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2023-12-08 11:58:22 +0000

    Function_2

--- func_2.js
+++ func_2.js
@@ -24,7 +24,7 @@ function HandleObject (anyObject){
 HandleObject(user)
 
 /*....................................................................................................*/
-/******************************also write object in function exicution**********************************/
+/******************************also get object values in function exicution**********************************/
 
 function HandleObject2 (anyobject2){
     console.log(`username is ${anyobject2.username2}, and User email is ${anyobject2.email2}`)
@@ -33,3 +33,21 @@ HandleObject2({
     username2 : "taimoor",
     email2 : "wwew@gmail.com"
 })
+
+/********************************************************************************************************/
+const arr = [ 200, 400, 600, 8000 ]
+function GetArrSecoundValue (Getarr){
+    return Getarr[0]
+}
+console.log(GetArrSecoundValue(arr))
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+/****************************Array values use in funcrion exicution***************************************/
+
+function getarrvalue (arrayvalue){
+    return arrayvalue[0]
+}
+console.log(getarrvalue([23654, 58464, 574812]))
+
+/********************************************************************************************************/
+
