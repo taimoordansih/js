4a8fb45365f7d08ee8e1c813e1dfa53b2d9bc7c0
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2023-12-08 12:41:53 +0000

    ArrowFunction

---
+++ ArrowFunction.js
@@ -0,0 +1,11 @@
+/*"This" function agar {} ke andar hoga to wo {} ke ader wali value ko refer kare ga
+Lekin agar bahar node enviroment mein "this" function type karein ge to khali object {} output hoga
+lekin browser mein global enviroment window hota h waha par kafi function atey h*/
+const user = {
+    username : "danish",
+    price : 999,
+
+    welcomeMessege : function (){
+        
+    }
+}
