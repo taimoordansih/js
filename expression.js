dd23de458a99e777da762149ff11f86754cea2c2
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2023-12-08 12:14:02 +0000

    Expression

---
+++ expression.js
@@ -0,0 +1,12 @@
+function addone (num){
+    return num + 1
+}
+console.log(addone(5))
+
+/*-----------------------------------------------------------------------------------------------------*/
+/*----------------------------------------------Expression---------------------------------------------*/
+const addTwo = function (num){
+    return num + 2
+}
+console.log(addTwo(6))
+/*If console.log(addTwo(6)) write before expression = error*/
